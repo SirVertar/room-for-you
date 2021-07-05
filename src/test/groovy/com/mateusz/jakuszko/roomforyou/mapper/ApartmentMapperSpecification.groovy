b3b32c908227ce39ab1d01516ea4126e33a81c1c
SirVertar <mateusz.jakuszko@gmail.com>
2021-07-05 16:10:20 +0200

    Refactor ApartmentMapperSpecification

--- src/test/groovy/com/mateusz/jakuszko/roomforyou/mapper/ApartmentMapperSpecification.groovy
+++ src/test/groovy/com/mateusz/jakuszko/roomforyou/mapper/ApartmentMapperSpecification.groovy
@@ -14,6 +14,40 @@ class ApartmentMapperSpecification extends Specification {
     @Autowired
     private ApartmentMapper apartmentMapper
 
+    def "map Apartment to ApartmentDto class"() {
+        given:
+        Apartment apartment = createApartment()
+
+        when:
+        ApartmentDto apartmentDto = apartmentMapper.mapToApartmentDto(apartment)
+
+        then:
+        checkIfFieldsAreTheSame(apartment, apartmentDto)
+    }
+
+    def "map ApartmentDto to Apartment class"() {
+        given:
+        ApartmentDto apartmentDto = createApartmentDto()
+
+        when:
+        Apartment apartment = convertApartmentDtoToApartment(apartmentDto)
+
+        then:
+        checkIfFieldsAreTheSame(apartment, apartmentDto)
+    }
+
+    def "map apartments list to apartmentsDto list"() {
+        given:
+        List<Apartment> apartments = createApartmentsList()
+
+        when:
+        def apartmentDtos = apartmentMapper.mapToApartmentDtos(apartments)
+
+        then:
+        apartmentDtos.size() == 1
+        checkIfFieldsAreTheSame(apartments.get(0), apartmentDtos.get(0))
+    }
+
     private static Apartment createApartment() {
         return Apartment.builder()
                 .id(1L)
@@ -26,6 +60,12 @@ class ApartmentMapperSpecification extends Specification {
                 .reservations(Collections.singletonList(Reservation.builder().id(11L).build()))
                 .customer(Customer.builder().id(12L).build())
                 .build()
+    }
+
+    private static List<Apartment> createApartmentsList() {
+        Apartment apartment = createApartment()
+        List<Apartment> apartments = [apartment]
+        return apartments
     }
 
     private static ApartmentDto createApartmentDto() {
@@ -42,77 +82,23 @@ class ApartmentMapperSpecification extends Specification {
                 .build()
     }
 
-    def "assert bean creation"() {
-        expect:
-        apartmentMapper != null
-    }
-
-    def "map Apartment to ApartmentDto class"() {
-
-        given: "create apartment"
-        Apartment apartment = createApartment()
-
-        when: "map apartment to apartmentDto"
-        ApartmentDto apartmentDto = apartmentMapper.mapToApartmentDto(apartment)
-
-        then: "assert all fields"
-        apartment.getApartmentNumber() == apartmentDto.getApartmentNumber()
-        apartment.getCity() == apartmentDto.getCity()
-        apartment.getCustomer().getId() == apartmentDto.getCustomerId()
-        apartment.getId() == apartmentDto.getId()
-        apartment.getLatitude() == apartmentDto.getLatitude()
-        apartment.getLongitude() == apartmentDto.getLongitude()
-        apartment.getLongitude() == apartmentDto.getLongitude()
-        apartment.getStreet() == apartmentDto.getStreet()
-        apartment.getStreetNumber() == apartmentDto.getStreetNumber()
-        apartment.getReservations().get(0).getId() == apartmentDto.getReservationsIds().get(0)
-    }
-
-    def "map ApartmentDto to Apartment class"() {
-
-        given: "create apartment"
-        ApartmentDto apartmentDto = createApartmentDto()
-
-
-        when: "map apartment to apartmentDto"
-        Apartment apartment = apartmentMapper.mapToApartment(
+    private Apartment convertApartmentDtoToApartment(ApartmentDto apartmentDto) {
+        apartmentMapper.mapToApartment(
                 apartmentDto,
                 Collections.singletonList(Reservation.builder().id(11L).build()),
                 Customer.builder().id(12L).build())
-
-        then: "assert all fields"
-        apartment.getApartmentNumber() == apartmentDto.getApartmentNumber()
-        apartment.getCity() == apartmentDto.getCity()
-        apartment.getCustomer().getId() == apartmentDto.getCustomerId()
-        apartment.getId() == apartmentDto.getId()
-        apartment.getLatitude() == apartmentDto.getLatitude()
-        apartment.getLongitude() == apartmentDto.getLongitude()
-        apartment.getLongitude() == apartmentDto.getLongitude()
-        apartment.getStreet() == apartmentDto.getStreet()
-        apartment.getStreetNumber() == apartmentDto.getStreetNumber()
-        apartment.getReservations().get(0).getId() == apartmentDto.getReservationsIds().get(0)
     }
 
-    def "map apartments list to apartmentsDto list"() {
-        given: "create apartments list"
-        Apartment apartment = createApartment()
-        List<Apartment> apartments = new ArrayList<>()
-        apartments << apartment
-
-        when: "map apartment list to apartmentDto list"
-        def apartmentDtos = apartmentMapper.mapToApartmentDtos(apartments)
-
-        then: "assert all fields"
-        apartmentDtos.size() == 1
-        apartment.getApartmentNumber() == apartmentDtos.get(0).getApartmentNumber()
-        apartment.getCity() == apartmentDtos.get(0).getCity()
-        apartment.getCustomer().getId() == apartmentDtos.get(0).getCustomerId()
-        apartment.getId() == apartmentDtos.get(0).getId()
-        apartment.getLatitude() == apartmentDtos.get(0).getLatitude()
-        apartment.getLongitude() == apartmentDtos.get(0).getLongitude()
-        apartment.getLongitude() == apartmentDtos.get(0).getLongitude()
-        apartment.getStreet() == apartmentDtos.get(0).getStreet()
-        apartment.getStreetNumber() == apartmentDtos.get(0).getStreetNumber()
-        apartment.getReservations().get(0).getId() == apartmentDtos.get(0).getReservationsIds().get(0)
+    private static void checkIfFieldsAreTheSame(Apartment apartment, ApartmentDto apartmentDto) {
+        assert apartment.getApartmentNumber() == apartmentDto.getApartmentNumber()
+        assert apartment.getCity() == apartmentDto.getCity()
+        assert apartment.getCustomer().getId() == apartmentDto.getCustomerId()
+        assert apartment.getId() == apartmentDto.getId()
+        assert apartment.getLatitude() == apartmentDto.getLatitude()
+        assert apartment.getLongitude() == apartmentDto.getLongitude()
+        assert apartment.getLongitude() == apartmentDto.getLongitude()
+        assert apartment.getStreet() == apartmentDto.getStreet()
+        assert apartment.getStreetNumber() == apartmentDto.getStreetNumber()
+        assert apartment.getReservations().get(0).getId() == apartmentDto.getReservationsIds().get(0)
     }
 }
